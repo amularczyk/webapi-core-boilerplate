cb488bf1c8490c5cd5e6cde14bfe667d3e80d56a
amularczyk <amularczyk@pgs-soft.com>
2019-09-04 09:09:16 +0200

    Core -> Adding unit tests.

---
+++ ProjectName/ProjectName.Core.Tests/ItemsReadServiceTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMoqCore;
+using Moq;
+using ProjectName.Core.Interfaces.Repositories;
+using ProjectName.Core.Models;
+using ProjectName.Core.Services;
+using Shouldly;
+using Xunit;
+
+namespace ProjectName.Core.Tests
+{
+    public class ItemsReadServiceTests
+    {
+        private readonly AutoMoqer _mocker;
+
+        public ItemsReadServiceTests()
+        {
+            _mocker = new AutoMoqer();
+        }
+
+        [Fact]
+        public async Task RetrieveAllAsync_ShouldReturnAllItems()
+        {
+            // Arrange
+            var items = new[] { new Item() };
+
+            var sut = _mocker.Create<ItemsReadService>();
+
+            var repositoryMock = _mocker.GetMock<IItemsRepository>();
+            repositoryMock.Setup(s => s.RetrieveAllAsync()).ReturnsAsync(items);
+
+            // Act
+            var result = (await sut.RetrieveAllAsync()).ToList();
+
+            // Assert
+            result.Count.ShouldBe(items.Length);
+            result[0].ShouldBe(items[0]);
+            repositoryMock.Verify(v => v.RetrieveAllAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task RetrieveByIdAsync_ShouldReturnItemForGivenId()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var item = new Item { Id = id };
+
+            var sut = _mocker.Create<ItemsReadService>();
+
+            var repositoryMock = _mocker.GetMock<IItemsRepository>();
+            repositoryMock.Setup(s => s.RetrieveByIdAsync(It.IsAny<Guid>())).ReturnsAsync(item);
+
+            // Act
+            var result = await sut.RetrieveByIdAsync(id);
+
+            // Assert
+            result.ShouldBe(item);
+            repositoryMock.Verify(v => v.RetrieveByIdAsync(It.Is<Guid>(p => p == id)), Times.Once);
+        }
+    }
+}
---
+++ ProjectName/ProjectName.Core.Tests/ItemsWriteServiceTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using AutoMoqCore;
+using Moq;
+using ProjectName.Core.Interfaces.Repositories;
+using ProjectName.Core.Models;
+using ProjectName.Core.Services;
+using Shouldly;
+using Xunit;
+
+namespace ProjectName.Core.Tests
+{
+    public class ItemsWriteServiceTests
+    {
+        private readonly AutoMoqer _mocker;
+
+        public ItemsWriteServiceTests()
+        {
+            _mocker = new AutoMoqer();
+        }
+
+        [Fact]
+        public async Task RetrieveByIdAsync_ShouldReturnItemForGivenId()
+        {
+            // Arrange
+            var item = new Item();
+
+            var sut = _mocker.Create<ItemsWriteService>();
+
+            var repositoryMock = _mocker.GetMock<IItemsRepository>();
+            repositoryMock.Setup(s => s.InsertAsync(It.IsAny<Item>())).ReturnsAsync(true);
+
+            // Act
+            var result = await sut.InsertAsync(item);
+
+            // Assert
+            item.Id.ShouldNotBe(Guid.Empty);
+            result.ShouldBe(item.Id);
+            repositoryMock.Verify(v => v.InsertAsync(It.Is<Item>(p => p == item)), Times.Once);
+        }
+    }
+}
